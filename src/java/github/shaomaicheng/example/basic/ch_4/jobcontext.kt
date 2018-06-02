cbcdddc17c380cf912810791fcccd6087347babf
chenglei <lpssycl@163.com>
2018-06-03 01:06:55 +0800

    更新readme

--- src/java/github/shaomaicheng/example/basic/ch_4/jobcontext.kt
+++
@@ -1,8 +0,0 @@
-package github.shaomaicheng.example.basic.ch_4
-
-import kotlinx.coroutines.experimental.Job
-import kotlinx.coroutines.experimental.runBlocking
-
-fun main(args: Array<String>) = runBlocking<Unit> {
-    println("My job is ${coroutineContext[Job]}")
-}
